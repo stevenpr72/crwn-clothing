0905f44dd6808acb9f097bf4810cecb7cbe3bfac
Steven Lam <steven.lam@zii.aero>
2022-09-20 09:31:28 -0700

    updating categorymenu page with category menu component and category item component

---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from '../category-item/category-item.component';
+
+import './category-menu.styles.scss';
+
+const CategoryMenu = ({categories}) => {
+
+
+    return (
+    <div className="directory-container">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    )
+}
+
+export default CategoryMenu;
