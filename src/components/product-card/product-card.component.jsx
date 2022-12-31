5a12a2963420eef8a667017b78ccca2a925c2a61
stevenpr72 <stevenprkhs@gmail.com>
2022-12-30 22:15:18 -0800

    cart dropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
+
 import './product-card.styles.scss'
 import Button from '../button/button.component';
 
+import { CartDropdownContext } from '../../contexts/cart-dropdown.context';
+
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartDropdownContext);
+
+    const addProductToCart = () => {
+        addItemToCart(product);
+    }
 
     return (
         <div className="product-card-container">
@@ -11,7 +20,7 @@ const ProductCard = ({product}) => {
                 <span className='name'>{name}</span>
                 <span className='cost'>{price}</span>
             </div>
-            <Button buttonType="inverted">Add to Cart</Button>
+            <Button buttonType="inverted" onClick={addProductToCart} >Add to Cart</Button>
         </div>
     )
 }
